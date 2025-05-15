a1d9ff6c917e22ebc039ab250b643f4fd5b25bce
Your Name <you@example.com>
2025-05-15 08:35:55 +0000

    just removed comments

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,11 @@
 int
 main(int argc, char *argv[])
 {
-  const unsigned int degree         = 1;
-  const double T      = 2.0;         // Longer simulation time
+  const unsigned int degree = 1;
+  const double T      = 1.0;
   const double deltat = 0.05;
-  const double theta  = 0.5;         // Change to Crank-Nicolson scheme
-  const unsigned int n_refinements = 2; // Start with less global refinements to make adaptivity more visible
+  const double theta  = 0.5;
+  const unsigned int n_refinements = 2;
 
   Heat problem(degree, T, deltat, theta, n_refinements);
 
